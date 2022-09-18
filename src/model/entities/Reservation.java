bfd4ac2a3586c1057b364a7a4437dffc6b723c5c
Gabriel Macedo <gabriel.ol.macedo@hotmail.com>
2022-09-18 11:37:06 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,19 +5,25 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	
 	private Integer roomNumber;
-	private Date checkin;
-	private Date checkout;
+	private Date checkIn;
+	private Date checkOut;
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	
-	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if(!checkOut.after(checkIn)){
+			throw new DomainException("Check-out date must be after check-in date"); 
+			
+		}
 		this.roomNumber = roomNumber;
-		this.checkin = checkin;
-		this.checkout = checkout;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
 
 
@@ -29,36 +35,35 @@ public class Reservation {
 		this.roomNumber = roomNumber;
 	}
 
-	public Date getCheckin() {
-		return checkin;
+	public Date getCheckIn() {
+		return checkIn;
 	}
 
 	
-	public Date getCheckout() {
-		return checkout;
+	public Date getCheckOut() {
+		return checkOut;
 	}
 	
 	public long duration() {
 		
-		long diff = checkout.getTime() - checkin.getTime();
+		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String uppDates(Date checkIn, Date checkOut) {
+	public void uppDates(Date checkIn, Date checkOut){
 		
 		Date now = new Date();
 		
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future date";
+			throw new DomainException("Reservation dates for update must be future date");
 		}	
 		if(!checkOut.after(checkIn)){
-			return "Check-out date must be after check-in date";
+			throw new DomainException("Check-out date must be after check-in date"); 
 			
 		}
 		
-		this.checkin = checkIn;
-		this.checkout = checkOut;
-		return null;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
 
 
@@ -67,9 +72,9 @@ public class Reservation {
 		return "Room"
 				+ roomNumber
 				+ ", check-in: "
-				+ sdf.format(checkin)
+				+ sdf.format(checkIn)
 				+ ", check-out: "
-				+ sdf.format(checkout)
+				+ sdf.format(checkOut)
 				+", "
 				+ duration()
 				+ " nights";
